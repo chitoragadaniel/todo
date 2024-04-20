eb06d340fffc5df3ca63a50c2f8527e28cef1d11
Chitoraga D <d.chitoraga@student.utwente.nl>
2024-04-20 13:25:34 +0200

    More refactoring

--- resources/views/components/add-task.blade.php
+++ resources/views/components/add-task.blade.php
@@ -5,21 +5,25 @@
         <label>
             Name
             <input type="text" name="name" required
-                   class="bg-black border-2 border-gray-300 rounded-md float-right">
+                   class="bg-black border-2 border-gray-300 rounded-md float-right ml-2">
         </label>
     </div>
     <div class="mt-2">
         <label>
             Description
             <input type="text" name="description"
-                   class="bg-black border-2 border-gray-300 rounded-md float-right">
+                   class="bg-black border-2 border-gray-300 rounded-md float-right ml-2">
         </label>
     </div>
     <div class="mt-2">
         <label>
             Label
-            <input type="text" name="label" class="bg-black border-2 border-gray-300 rounded-md float-right">
+            <input type="text" name="label"
+                   class="bg-black border-2 border-gray-300 rounded-md float-right ml-2">
         </label>
     </div>
-    <button type="submit" class="mt-2 border-2 border-gray-300 rounded-md w-16"> Add</button>
+    <div class="mt-2">
+        <x-button>Add</x-button>
+    </div>
+
 </form>
--- resources/views/components/task-list.blade.php
+++ resources/views/components/task-list.blade.php
@@ -4,13 +4,18 @@
     <h3 class="text-2xl m-4">{{ $slot }}</h3>
     <div class="w-1/4">
         @foreach($tasks as $task)
-            <div class="flex justify-between w-full">
+            <div class="flex justify-between w-full mt-1">
                 <span class="m-1"> <a href="/{{ $task->id}}"> {{ $task->name }} </a> </span>
-                <form method="post" action="/{{$task->id}}">
-                    @csrf
-                    @method($method)
-                    <button class="submit"> {{$button}}</button>
-                </form>
+                <div class="flex">
+                    @if($task->label)
+                        <span class="mr-3 text-red-500">#{{$task->label}}</span>
+                    @endif
+                    <form method="post" action="/{{$task->id}}">
+                        @csrf
+                        @method($method)
+                        <x-button>{{$button}}</x-button>
+                    </form>
+                </div>
             </div>
         @endforeach
     </div>
